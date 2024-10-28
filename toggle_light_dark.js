9b419d655df2e2be5e4d279e458700551f3b2b85
kawarimidoll <kawarimidoll+git@gmail.com>
2024-10-29 09:35:20 +1100

    feat: flexible toggle light and dark

--- toggle_light_dark.js
+++ toggle_light_dark.js
@@ -3,15 +3,38 @@ const root = document.documentElement;
 function getRootProperty(name) {
   return getComputedStyle(root).getPropertyValue(name).trim();
 }
-function swapRootProperty(left, right) {
-  const leftProp = getRootProperty(left);
-  const rightProp = getRootProperty(right);
-  root.style.setProperty(right, leftProp);
-  root.style.setProperty(left, rightProp);
+
+const rootCssVariables = Array.from(document.styleSheets)
+  .flatMap((styleSheet) => Array.from(styleSheet.cssRules))
+  .filter(
+    (cssRule) =>
+      cssRule instanceof CSSStyleRule && cssRule.selectorText === ":root",
+  )
+  .flatMap((cssRule) => Array.from(cssRule.style))
+  .filter((style) => style.startsWith("--"));
+
+const toggleTargets = [];
+for (const cssVar of rootCssVariables) {
+  if (!cssVar.startsWith("--light-")) {
+    continue;
+  }
+  const key = cssVar.replace("--light-", "");
+  const darkValue = getRootProperty(`--dark-${key}`);
+  if (!darkValue) {
+    continue;
+  }
+  toggleTargets.push(key);
 }
 
 const avagarImg = document.querySelector(".avatar img");
 avagarImg.addEventListener("click", () => {
-  swapRootProperty("--light-color", "--dark-color");
-  swapRootProperty("--light-gray", "--dark-gray");
+  for (const key of toggleTargets) {
+    // swap light and dark
+    const lightKey = `--light-${key}`;
+    const darkKey = `--dark-${key}`;
+    const lightValue = getRootProperty(lightKey);
+    const darkValue = getRootProperty(darkKey);
+    root.style.setProperty(lightKey, darkValue);
+    root.style.setProperty(darkKey, lightValue);
+  }
 });
