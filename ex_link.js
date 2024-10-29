19099295a8255036d5e5080ba6cf3898ea71bffa
kawarimidoll <kawarimidoll+git@gmail.com>
2024-10-29 21:12:45 +1100

    refactor: simplify web components

--- ex_link.js
+++ ex_link.js
@@ -1,14 +1,12 @@
 class ExLink extends HTMLElement {
   constructor() {
     super();
-    const shadow = this.attachShadow({ mode: "open" });
     const url = this.textContent;
     const link = document.createElement("a");
-    link.setAttribute("part", "a");
     link.href = url;
     link.target = "_blank";
     link.textContent = this.getAttribute("text") || url;
-    shadow.appendChild(link);
+    this.replaceWith(link);
   }
 }
 customElements.define("ex-link", ExLink);
