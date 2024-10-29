1be25d6239e76a1e23b19f564246cd8b31e1aaf2
kawarimidoll <kawarimidoll+git@gmail.com>
2024-10-29 15:59:21 +1100

    feat: support dark mode of link tags

--- ex_link.js
+++ ex_link.js
@@ -4,10 +4,10 @@ class ExLink extends HTMLElement {
     const shadow = this.attachShadow({ mode: "open" });
     const url = this.textContent;
     const link = document.createElement("a");
+    link.setAttribute("part", "a");
     link.href = url;
     link.target = "_blank";
     link.textContent = this.getAttribute("text") || url;
-    link.style = "word-break: break-all;";
     shadow.appendChild(link);
   }
 }
